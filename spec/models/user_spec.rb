cf4a00bd64e334b7a8766775a3870161512c8a3b
Matt Spiegel <matt@mycaseinc.com>
2012-05-20 20:26:33 -0700

    Finished first cut of the User model

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,62 @@
+require 'spec_helper'
+
+describe User do
+  
+  before(:each) do
+    @attr = {:name => "Example User", :email => "user@example.com"}
+  end
+  
+  it "should create a new user with validated attributes" do
+    User.create!(@attr)
+  end
+  
+  it "should require a name" do
+    no_name_user = User.new(@attr.merge(:name=> ""))
+    no_name_user.should_not be_valid
+  end
+  
+  it "should require an email" do
+    no_email_user = User.new(@attr.merge(:email=> ""))
+    no_email_user.should_not be_valid
+  end
+  
+  it "should reject names that are too long" do
+    long_name = "a" * 51
+    long_name_user = User.new(@attr.merge(:name => long_name))
+    long_name_user.should_not be_valid
+  end
+  
+  it "should allow emails that are formatted correctly" do
+    addresses = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
+    addresses.each do |address|
+      valid_email_user = User.new(@attr.merge(:email => address))
+      valid_email_user.should be_valid
+    end
+  end
+  
+  it "should not allow emails that are not formatted correctly" do
+    addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
+    addresses.each do |address|
+      valid_email_user = User.new(@attr.merge(:email => address))
+      valid_email_user.should_not be_valid
+    end
+  end
+  
+  it "should only allow one email to be used in system" do
+    User.create!(@attr)
+    user_with_duplicate_email = User.new(@attr)
+    user_with_duplicate_email.should_not be_valid
+  end
+end
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
